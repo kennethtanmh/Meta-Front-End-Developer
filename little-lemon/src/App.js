b4dcd36b05a38030547b988ace920522d0c178c3
kennethtanmh <kennethtanmh99@gmail.com>
2023-07-23 16:13:11 +0800

    navigation bar done

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,14 +1,12 @@
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Nav />
       <Main />
       <Footer />
     </>
--- little-lemon/src/components/Header/Header.js
+++ little-lemon/src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
+import logo from '../../img/Logo.svg'
+import './styles.css'
 
-export class Nav extends Component {
+export class Header extends Component {
   render() {
     return (
-      <ul>
-        <li><a href="/"></a>Home</li>
+      <header>
+      <ul className='navbar'>
+        <li className='logo'>
+          <img src={logo} alt="Little Lemon Logo" />
+        </li>
+        <li><a href="/">Home</a></li>
         <li><a href='/About'>About</a></li>
         <li><a href='/Menu'>Menu</a></li>
         <li><a href='/Reservations'>Reservations</a></li>
         <li><a href='/Order'>Order Online</a></li>
         <li><a href='/Login'>Login</a></li>
       </ul>
+    </header>
     )
   }
 }
-
-export default Nav
+export default Header
