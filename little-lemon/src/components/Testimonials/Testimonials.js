72b9981105fad9a68724acd1d3765891968d7071
kennethtanmh <kennethtanmh99@gmail.com>
2023-07-24 23:20:24 +0800

    fixed the display for testimonial card

--- little-lemon/src/components/Testimonials/Testimonials.js
+++ little-lemon/src/components/Testimonials/Testimonials.js
@@ -6,25 +6,25 @@ const reviews = [
     id: 1,
     name: 'Marcus Rashford',
     comment:
-      'The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+      '"The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."',
   },
   {
     id: 2,
     name: 'Bruno Fernandes',
     comment:
-      'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive.',
+      '"Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive."',
   },
   {
     id: 3,
     name: 'Lisandro Martínez',
     comment:
-      'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+      '"This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."',
   },
   {
     id: 4,
     name: 'Harry Maguire',
     comment:
-      'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+      '"This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."',
   },
 ];
 
@@ -32,7 +32,7 @@ const reviews = [
 const Testimonials = () => {
   const playerReviews = reviews.map(({ id, name , comment }) => {
     return (
-      <div key={id} className='testimonials-map-container'>
+        <div key={id} className='testimonials-map-container'>
         <p className='testimonials-name'>{name}</p>
         <p className='testimonials-description'>{comment}</p>
       </div>
@@ -44,7 +44,7 @@ const Testimonials = () => {
       <div className='testimonials-header'>
         <h1 className='testimonials-title'>Elevating Culinary Experiences: Hear from Our Cherished Guests!</h1>
       </div>
-      <div className='testimonals-cards'>{playerReviews}</div>
+      <div className='testimonials-card'>{playerReviews}</div>
     </section>
   )
 }
