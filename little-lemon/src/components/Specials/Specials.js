cc8264d63fb5f954d8a1c8e5938659c68af2ac49
kennethtanmh <kennethtanmh99@gmail.com>
2023-07-25 16:15:53 +0800

    changed the menu button

--- little-lemon/src/components/Specials/Specials.js
+++ little-lemon/src/components/Specials/Specials.js
@@ -6,7 +6,6 @@ const Specials = () => {
   return (
     <section className='specials'>
       <div className='specials-header'>
-        <h1 className='specials-title'>This weeks specials!</h1>
         <button className='menu'>Online Menu</button>
       </div>
       <SpecialCard />
